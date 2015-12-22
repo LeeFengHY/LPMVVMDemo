160cceba9ac6e832079536adadd27b3614f44fa7
LeeFengHY <578545715@qq.com>
2015-12-22 10:40:47 +0800

    Initial Commit

---
+++ LPMVVMDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LPMVVMDemo
+//
+//  Created by QFWangLP on 15/12/22.
+//  Copyright © 2015年 QFWang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LPMVVMDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LPMVVMDemo
+//
+//  Created by QFWangLP on 15/12/22.
+//  Copyright © 2015年 QFWang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
